0495dc40b1801fbb4d40787ed2ca425784c3dd5c
Loic Botrel <loic.botrel@gmail.com>
2021-08-17 03:59:20 +0200

    pypi support excepted for meegkit

---
+++ README.rst
@@ -0,0 +1,7 @@
+p3k
+--------
+
+To use (with caution), simply do::
+
+    >>> import p3k
+    >>> p3k.run_analysis()
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'p3k - yet another P300 offline classification tool'
+LONG_DESCRIPTION = 'P300 offline classification from BCI2000 P3Speller and OpenVibe P300 scenario'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="p3k",
+    version=VERSION,
+    author="Loic Botrel",
+    author_email="<loic.botrel@uni-wuerzburg.de>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['sklearn', 'pandas', 'seaborn', 'numpy', 'mne', 'matplotlib', 'BCI2kReader', 'statsmodels', 'pyriemann',
+	'meegkit @ git+https://github.com/nbara/python-meegkit.git#egg=meegkit[extra]'],
+	#dependency_links=['git+https://github.com/nbara/python-meegkit.git#egg=meegkit'],
+	python_requires='>3.8.1',
+	
+    keywords=['python', 'bci2000', 'p300', 'speller', 'bci', 'bmi', 'eeg', 'openvibe', 'lda', 'mne'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+	license='MIT',
+	url='https://github.com/lokinou/p300_analysis_from_openvibe_and_bci2000'
+)
